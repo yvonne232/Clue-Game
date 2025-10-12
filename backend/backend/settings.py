786dca554fc43aa4f0de5694ef4ca9f23a820637
yvonne232 <91641067+yvonne232@users.noreply.github.com>
2025-10-11 21:25:27 -0400

    Added frontend, cors origin, and django rest framework; moved files up from double backend folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "realtime",     # app that contains Websocket consumer
-    "channels"      # Django Chennels for WebsSockets/ASGI
+    "channels",     # Django Chennels for WebsSockets/ASGI
+    'rest_framework',
+    "corsheaders"
 
 ]
 
@@ -60,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -128,3 +131,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
